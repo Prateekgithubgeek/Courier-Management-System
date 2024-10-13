5fd66bc3ad4a7e2220cb5b90a5e5a254ca6c1dd2
Prateeksfreakygithub <112895919+Prateekgithubgeek@users.noreply.github.com>
2024-10-13 17:36:41 +0530

    This consist of updated codebase

---
+++ C#/C#/oops/CourierManagementSystem.BusinessLayer/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourierManagementSystem.Entity;
+
+namespace CourierManagementSystem.BusinessLayer.Repository
+{
+    internal class UserRepository : IUserRepository
+    {
+        // Default constructor
+        public UserRepository() { }
+
+        //Parameterized constructor
+        User user = new User();
+        public UserRepository(int userID, string userName, string email, string password, string contactNumber, string address)
+        {
+            user.userID = userID;
+            user.userName = userName;
+            user.email = email;
+            user.password = password;
+            user.contactNumber = contactNumber;
+            user.address = address;
+
+        }
+        public override string ToString()
+        {
+            return $"User {{ userID={user.userID}, userName='{user.userName}', email='{user.email}', " +
+                   $"password='{user.password}', contactNumber=''{user.contactNumber}',address={user.address} }}";
+
+        }
+    }
+}
